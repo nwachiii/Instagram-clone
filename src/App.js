3630f39afed7b31336693d89ec27a2fcbc85732b
Emmanuel Nwachi <egreatness1@gmail.com>
2020-12-11 18:45:46 +0100

    'useSate hook for posts'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState, React } from "react";
 import Posts from "./Posts";
 import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "Happy Family",
+      caption: "Classy Bougie Catchy",
+      imageUrl:
+        "https://images.unsplash.com/photo-1484712401471-05c7215830eb?ixlib:rb-1.2.1&ixid:MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8&auto:format&fit:crop&w:500&q:60",
+    },
+    {
+      username: "Echelon Agency",
+      caption: "Workflow strategy for productive marketing",
+      imageUrl:
+        "https://images.unsplash.com/photo-1533749871411-5e21e14bcc7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fG1hcmtldGluZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      username: "ChessChampions_Chicago",
+      caption: "The game for the intellectuals",
+      imageUrl:
+        "https://images.unsplash.com/flagged/photo-1570551502611-c590dc45f644?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Y2hlc3N8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      username: "Jane Stone",
+      caption: "Smile Regardless",
+      imageUrl:
+        "https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib:rb-1.2.1&ixid:MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NTl8fHxlbnwwfHx8&auto:format&fit:crop&w:500&q:60",
+    },
+  ]);
+
   return (
     <div className="App">
       <div className="app__header">
@@ -11,26 +39,16 @@ function App() {
           alt=""
         />
       </div>
-      <Posts
-        username="Happy Family"
-        caption="Classy Bougie Catchy"
-        imageUrl="https://images.unsplash.com/photo-1484712401471-05c7215830eb?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60"
-      />
-      <Posts
-        username="Crissy"
-        caption="The beautiful art of nature"
-        imageUrl="https://scontent.flos3-2.fna.fbcdn.net/v/t1.0-9/106727869_735070363920254_6908071804355338326_o.jpg?_nc_cat=110&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeHgvdbjaWl8GMQgoFdhrEzALEhHwQXBm2ssSEfBBcGbazVVAG7VFMRiyDHivaGOG_EtV9s2XX3f9-NxtlYs5-KU&_nc_ohc=hcOyz64U-NIAX9o3giE&_nc_ht=scontent.flos3-2.fna&oh=4c4311f3e7d5ffa0ed7b6cb2c6d99713&oe=5FF834F0"
-      />
-      <Posts
-        username="Jane Stone"
-        caption="Smile Regardless"
-        imageUrl="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NTl8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60"
-      />
-      <Posts
-        username="Crissy"
-        caption="The beautiful art of nature"
-        imageUrl="https://scontent.flos3-2.fna.fbcdn.net/v/t1.0-9/106727869_735070363920254_6908071804355338326_o.jpg?_nc_cat=110&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeHgvdbjaWl8GMQgoFdhrEzALEhHwQXBm2ssSEfBBcGbazVVAG7VFMRiyDHivaGOG_EtV9s2XX3f9-NxtlYs5-KU&_nc_ohc=hcOyz64U-NIAX9o3giE&_nc_ht=scontent.flos3-2.fna&oh=4c4311f3e7d5ffa0ed7b6cb2c6d99713&oe=5FF834F0"
-      />
+
+      {posts.map((post) => {
+        return (
+          <Posts
+            username={post.username}
+            caption={post.caption}
+            imageUrl={post.imageUrl}
+          />
+        );
+      })}
     </div>
   );
 }
