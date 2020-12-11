a1fe56f0f6ab40a3f88abb6f3ffbffb252719301
Emmanuel Nwachi <egreatness1@gmail.com>
2020-12-11 16:25:02 +0100

    'set up posts component with props'

--- src/App.js
+++ src/App.js
@@ -10,14 +10,27 @@ function App() {
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt=""
         />
-        <h1>
-          HELLO Clever Emmanuel, Let's Build an Instagram Clone with React!
-        </h1>
       </div>
-
-      <Posts />
-      {/* posts */}
-      {/* posts */}
+      <Posts
+        username="Happy Family"
+        caption="Classy Bougie Catchy"
+        imageUrl="https://images.unsplash.com/photo-1484712401471-05c7215830eb?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60"
+      />
+      <Posts
+        username="Crissy"
+        caption="The beautiful art of nature"
+        imageUrl="https://scontent.flos3-2.fna.fbcdn.net/v/t1.0-9/106727869_735070363920254_6908071804355338326_o.jpg?_nc_cat=110&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeHgvdbjaWl8GMQgoFdhrEzALEhHwQXBm2ssSEfBBcGbazVVAG7VFMRiyDHivaGOG_EtV9s2XX3f9-NxtlYs5-KU&_nc_ohc=hcOyz64U-NIAX9o3giE&_nc_ht=scontent.flos3-2.fna&oh=4c4311f3e7d5ffa0ed7b6cb2c6d99713&oe=5FF834F0"
+      />
+      <Posts
+        username="Jane Stone"
+        caption="Smile Regardless"
+        imageUrl="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NTl8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60"
+      />
+      <Posts
+        username="Crissy"
+        caption="The beautiful art of nature"
+        imageUrl="https://scontent.flos3-2.fna.fbcdn.net/v/t1.0-9/106727869_735070363920254_6908071804355338326_o.jpg?_nc_cat=110&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeHgvdbjaWl8GMQgoFdhrEzALEhHwQXBm2ssSEfBBcGbazVVAG7VFMRiyDHivaGOG_EtV9s2XX3f9-NxtlYs5-KU&_nc_ohc=hcOyz64U-NIAX9o3giE&_nc_ht=scontent.flos3-2.fna&oh=4c4311f3e7d5ffa0ed7b6cb2c6d99713&oe=5FF834F0"
+      />
     </div>
   );
 }
--- src/Posts.js
+++ src/Posts.js
@@ -1,22 +1,25 @@
 import React from "react";
-
+import Avatar from "@material-ui/core/Avatar";
 import "./Posts.css";
 
-function Posts() {
+function Posts({ username, caption, imageUrl }) {
   return (
-    <div>
+    <div className="post">
       {/* header -> avatar + username */}
-      <h1>Username header</h1>
-
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt={username}
+          src="././public/profile-img.png"
+        />
+        <h3>{username}</h3>
+      </div>
       {/* Image */}
-      <img
-        className="post__image"
-        src="https://images.unsplash.com/photo-1604145559206-e3bce0040e2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
-      />
+      <img className="post__image" src={imageUrl} alt={username} />
 
       {/* username + caption */}
       <h4 className="post__text">
-        <strong>Emmanuel</strong> Wow this is a cool view
+        <strong>{username}: </strong> {caption}
       </h4>
     </div>
   );
